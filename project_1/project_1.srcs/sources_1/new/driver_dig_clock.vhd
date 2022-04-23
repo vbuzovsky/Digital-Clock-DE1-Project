4f340d954d71ee10fc79e9222bbf4d065dbf0dd8
Slimakk <miroslav.babela.jr@gmail.com>
2022-04-23 22:30:44 +0200

    23/04 - updated testbench for clock_setter

---
+++ project_1/project_1.srcs/sources_1/new/driver_dig_clock.vhd
@@ -0,0 +1,138 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.04.2022 17:08:23
+-- Design Name: 
+-- Module Name: driver_dig_clock - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity driver_dig_clock is
+     port(
+        clk  : in  std_logic;
+        rst  : in std_logic;
+        sw_i : in std_logic;
+        btn_1_i : in std_logic;
+        btn_2_i : in std_logic;
+        btn_3_i : in std_logic;
+        btn_4_i : in std_logic; -- BUTTON PRO PREPIS HODNOT V CLOCKU
+        h0_o : out std_logic_vector(3 downto 0);
+        h1_o : out std_logic_vector(3 downto 0);
+        m0_o : out std_logic_vector(3 downto 0);
+        m1_o : out std_logic_vector(3 downto 0);
+        s0_o : out std_logic_vector(3 downto 0);
+        s1_o : out std_logic_vector(3 downto 0)
+    );
+end driver_dig_clock;
+
+architecture Behavioral of driver_dig_clock is
+--Internal signals from p_mux to bcd_conv
+    signal s_secs  : std_logic_vector(5 downto 0);
+    signal s_mins : std_logic_vector(5 downto 0);
+    signal s_hrs : std_logic_vector(4 downto 0);
+-- Internal signals from digital_clock to p_mux
+    signal s_d_secs  : std_logic_vector(5 downto 0);
+    signal s_d_mins : std_logic_vector(5 downto 0);
+    signal s_d_hrs : std_logic_vector(4 downto 0);
+-- Internal signals from clock_setter to p_mux and dig_clock
+    signal s_c_secs  : std_logic_vector(5 downto 0):="000000";
+    signal s_c_mins : std_logic_vector(5 downto 0):= "000000";
+    signal s_c_hrs : std_logic_vector(4 downto 0):= "00000";
+begin
+    digital_clock : entity work.digital_clock
+        port map(
+            clk   => clk,
+            reset => rst,
+            
+            -- HODNOTY Z VYSTUPU clock_setter + BUTTON
+            set_button => btn_4_i,
+            set_second => s_c_secs,
+            set_minute => s_c_mins,
+            set_hour => s_c_hrs,
+            
+            second => s_d_secs,
+            minute => s_d_mins,
+            hour => s_d_hrs
+        );
+    
+    clock_setter: entity work.clock_setter
+        port map(
+           button_1 => btn_1_i,
+           button_2 => btn_2_i,
+           button_3 => btn_3_i,
+           sw => sw_i,
+           seconds_o => s_c_secs,
+           minutes_o => s_c_mins,
+           hours_o => s_c_hrs
+        );
+    
+    to_bcd_conv_s : entity work.to_bcd_conv
+        generic map(
+            g_CONV_WIDTH => 5
+        )
+        port map(
+            in_binary => s_secs,
+            digit_0 => s0_o,
+            digit_1 => s1_o
+        );
+    to_bcd_conv_m : entity work.to_bcd_conv
+        generic map(
+            g_CONV_WIDTH => 5
+        )
+        port map(
+            in_binary => s_mins,
+            digit_0 => m0_o,
+            digit_1 => m1_o
+        );
+    to_bcd_conv_h : entity work.to_bcd_conv
+        generic map(
+            g_CONV_WIDTH => 4
+        )
+        port map(
+            in_binary => s_hrs,
+            digit_0 => h0_o,
+            digit_1 => h1_o
+        );
+        
+        
+    --MUX FOR CLOCK_SETTER & DIG_CLOCK    
+    p_mux : process(clk)
+    begin
+        if rising_edge (clk) then
+            case sw_i is
+                when '1' =>
+                    s_secs <= s_c_secs;
+                    s_mins <= s_c_mins;
+                    s_hrs <= s_c_hrs;
+                when others =>
+                    s_secs <= s_d_secs;
+                    s_mins <= s_d_mins;
+                    s_hrs <= s_d_hrs;
+            end case;
+         end if;
+    end process p_mux;
+end Behavioral;
---
+++ project_1/project_1.srcs/sources_1/new/top.vhd
@@ -0,0 +1,131 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity top is
+    port(
+        CLK100MHZ     : in  std_logic;
+        BTNC  : in  std_logic;
+        
+        BTNU : in std_logic;
+        BTNL : in std_logic;
+        BTNR : in std_logic;
+        BTND : in std_logic;
+   
+        SW0 : in std_logic;
+      
+        CA : out STD_LOGIC;
+        CB : out STD_LOGIC;
+        CC : out STD_LOGIC;
+        CD : out STD_LOGIC;
+        CE : out STD_LOGIC;
+        CF : out STD_LOGIC;
+        CG : out STD_LOGIC;
+   
+        AN : out STD_LOGIC_VECTOR(7 downto 0)
+   
+    );
+end entity top;
+
+------------------------------------------------------------
+-- Architecture declaration for top
+------------------------------------------------------------
+architecture Behavioral of top is
+    signal s_h0  : std_logic_vector(3 downto 0);
+    signal s_h1  : std_logic_vector(3 downto 0);
+  
+    signal s_m0  : std_logic_vector(3 downto 0);
+    signal s_m1  : std_logic_vector(3 downto 0);
+    
+    signal s_s0  : std_logic_vector(3 downto 0);
+    signal s_s1  : std_logic_vector(3 downto 0);
+    
+    signal BTNU_deb : std_logic;
+    signal BTNL_deb : std_logic;
+    signal BTNR_deb : std_logic;
+    signal BTND_deb : std_logic;
+
+begin
+    BTNU_debouncer : entity work.button_debouncer
+        port map(
+            input => BTNU,
+            cclk => CLK100MHZ,
+            rst => BTNC,
+            output => BTNU_deb
+        );
+        
+    BTNL_debouncer : entity work.button_debouncer
+        port map(
+            input => BTNL,
+            cclk => CLK100MHZ,
+            rst => BTNC,
+            output => BTNL_deb
+        );
+        
+    BTNR_debouncer : entity work.button_debouncer
+        port map(
+            input => BTNR,
+            cclk => CLK100MHZ,
+            rst => BTNC,
+            output => BTNR_deb
+        );
+        
+    BTND_debouncer : entity work.button_debouncer
+        port map(
+            input => BTND,
+            cclk => CLK100MHZ,
+            rst => BTNC,
+            output => BTND_deb
+        );
+
+
+    driver_dig_clock : entity work.driver_dig_clock
+        port map(
+               clk => CLK100MHZ,
+               rst => BTNC,
+               
+               sw_i => SW0,
+               btn_1_i => BTNU_deb,
+               btn_2_i => BTNL_deb,
+               btn_3_i => BTNR_deb,
+               btn_4_i => BTND_deb,
+                
+               h0_o => s_h0,
+               h1_o => s_h1,
+               
+               m0_o => s_m0,
+               m1_o => s_m1,
+               
+               s0_o => s_s0,
+               s1_o => s_s1
+            );
+    
+    
+    driver_7seg_4digits : entity work.driver_7seg_4digits
+            port map(
+                clk => CLK100MHZ,
+                reset => BTNC,
+                
+                data_h0_i => s_h0,
+                data_h1_i => s_h1,
+                
+                data_m0_i => s_m0,
+                data_m1_i => s_m1,
+                
+                data_s0_i => s_s0,
+                data_s1_i => s_s1,
+                
+                seg_o(6) => CA,
+                seg_o(5) => CB,
+                seg_o(4) => CC,
+                seg_o(3) => CD,
+                seg_o(2) => CE,
+                seg_o(1) => CF,
+                seg_o(0) => CG,
+                
+                dig_o(5 downto 0) => AN(5 downto 0)
+            );
+    AN(7 downto 6) <= b"11";
+           
+end architecture Behavioral;
